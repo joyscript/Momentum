137b966868e98f8860cd583fb21646195bb4cf6a
Yana Joyscript <joyscript7@gmail.com>
2023-01-10 22:13:21 +0300

    feat: improve appearance of greeting

--- momentum/js/modules/storage.js
+++ momentum/js/modules/storage.js
@@ -1,11 +1,7 @@
 export const storage = () => {
   const name = document.querySelector('.name');
-  
+
   window.addEventListener('beforeunload', () => {
     localStorage.setItem('name', name.value);
-  });
-
-  window.addEventListener('load', () => {
-    name.value = localStorage.getItem('name');
   });
 };
--- momentum/js/modules/timer.js
+++ momentum/js/modules/timer.js
@@ -1,19 +1,13 @@
 export const timer = () => {
   const time = document.querySelector('.time');
   const date = document.querySelector('.date');
+  const name = document.querySelector('.name');
+  const hidName = document.querySelector('.hid-name');
   const greeting = document.querySelector('.greeting');
-  const timesOfDay = ['night', 'morning', 'afternoon', 'evening'];
 
   const now = new Date();
-
-  const showDate = () => {
-    date.textContent = now.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
-  };
-
-  const showGreeting = () => {
-    const partOfDay = Math.floor(now.getHours() / 6);
-    greeting.textContent = `Good ${timesOfDay[partOfDay]},`;
-  };
+  const partOfDay = Math.floor(now.getHours() / 6);
+  const timesOfDay = ['night', 'morning', 'afternoon', 'evening'];
 
   const showTime = () => {
     const nowString = new Date().toLocaleTimeString();
@@ -22,8 +16,28 @@ export const timer = () => {
     if (nowString.slice(0, 2) % 6 === 0) showGreeting();
   };
 
+  const showDate = () => {
+    date.textContent = now.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+  };
+
+  const showGreeting = () => {
+    greeting.textContent = `Good ${timesOfDay[partOfDay]},`;
+  };
+
+  const showName = () => {
+    name.value = hidName.textContent = localStorage.getItem('name');
+    setTimeout(() => hidName.classList.add('trans'), 600);
+    checkName();
+  };
+
+  const checkName = () => (name.value ? name.classList.add('filled') : name.classList.remove('filled'));
+
   showTime();
   showDate();
+  showName();
   showGreeting();
   setInterval(showTime, 1000);
+
+  name.addEventListener('input', () => (hidName.textContent = name.value));
+  name.addEventListener('change', checkName);
 };
