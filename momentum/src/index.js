70918bc3670b8f567439af6692f2082b87a70012
Yana Joyscript <joyscript7@gmail.com>
2023-02-26 21:02:33 +0300

    feat: add webpack, change the structure of the project

---
+++ momentum/src/index.js
@@ -0,0 +1,18 @@
+import './index.html';
+import './style.scss';
+
+import { showBackground } from './js/slider';
+import { showTimeAndGreeting } from './js/timer';
+import { showWeather } from './js/weather';
+import { showQuote } from './js/quote';
+import { showPlayer } from './js/player';
+import { showMantra } from './js/mantra';
+import { setSettings } from './js/settings';
+
+showBackground();
+showTimeAndGreeting();
+showWeather();
+showQuote();
+showPlayer();
+showMantra();
+setSettings();
--- momentum/src/js/audioData.js
+++ momentum/src/js/audioData.js
@@ -2,36 +2,36 @@ export const audioData = [
   {
     title: 'Circle of life',
     singer: 'E.John, L.Mkhize, Lebo M',
-    src: '../assets/sounds/Circle_of_life.mp3',
+    src: '../data/sounds/Circle_of_life.mp3',
   },
   {
     title: 'Adventure of a lifetime',
     singer: 'Coldplay',
-    src: '../assets/sounds/Adventure_of_a_lifetime.mp3',
+    src: '../data/sounds/Adventure_of_a_lifetime.mp3',
   },
   {
     title: 'Enjoy the silence',
     singer: 'Depeche Mode',
-    src: '../assets/sounds/Enjoy_the_silence.mp3',
+    src: '../data/sounds/Enjoy_the_silence.mp3',
   },
   {
     title: 'Leben... I feel you',
     singer: 'Schiller, Peter Heppner',
-    src: '../assets/sounds/Leben_I_feel_you.mp3',
+    src: '../data/sounds/Leben_I_feel_you.mp3',
   },
   {
     title: 'Wonderful life',
     singer: 'Hurts',
-    src: '../assets/sounds/Wonderful_life.mp3',
+    src: '../data/sounds/Wonderful_life.mp3',
   },
   {
     title: 'Return to innocence',
     singer: 'Enigma',
-    src: '../assets/sounds/Return_to_innocence.mp3',
+    src: '../data/sounds/Return_to_innocence.mp3',
   },
   {
     title: 'Porcelain',
     singer: 'Moby',
-    src: '../assets/sounds/Porcelain.mp3',
+    src: '../data/sounds/Porcelain.mp3',
   },
 ];
